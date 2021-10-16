92e115239a1f2a180e565ab1c0f6992fbc7d857a
Damian Kreft <damiankreft87@gmail.com>
2021-10-16 13:13:36 +0200

    Add UUID. Fix compile error in getCalories method.

--- src/main/java/pl/edu/pjatk/pyatkway/pyatkway/models/Sandwich.java
+++ src/main/java/pl/edu/pjatk/pyatkway/pyatkway/models/Sandwich.java
@@ -1,16 +1,23 @@
 package pl.edu.pjatk.pyatkway.pyatkway.models;
 
 import java.util.List;
+import java.util.UUID;
 
 public class Sandwich {
+    private final UUID guid;
     private String name;
     private List<Ingredient> ingredients;
     private double price;
-
-    public Sandwich(String name, List<Ingredient> ingredients, double price) {
+    
+    public Sandwich(UUID guid, String name, List<Ingredient> ingredients, double price) {
+        this.guid = guid;
         this.name = name;
         this.ingredients = ingredients;
         this.price = price;
+    }
+
+    public UUID getGuid() {
+        return guid;
     }
 
     public String getName() {
@@ -42,5 +49,7 @@ public class Sandwich {
         for (var i : ingredients) {
             caloriesTotal += i.getCalories();
         }
+
+        return caloriesTotal;
     }
 }
