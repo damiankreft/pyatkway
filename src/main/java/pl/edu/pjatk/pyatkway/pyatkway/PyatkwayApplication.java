c6987cf95d1f6d629baf6423189f862a2cf5d55d
Damian Kreft <damiankreft87@gmail.com>
2021-10-16 12:27:07 +0200

    Add project files.

---
+++ src/main/java/pl/edu/pjatk/pyatkway/pyatkway/PyatkwayApplication.java
@@ -0,0 +1,14 @@
+package pl.edu.pjatk.pyatkway.pyatkway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyatkwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PyatkwayApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
---
+++ src/test/java/pl/edu/pjatk/pyatkway/pyatkway/PyatkwayApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pjatk.pyatkway.pyatkway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PyatkwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
